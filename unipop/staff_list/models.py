848cf8ab39abb7ea1029c3b3cbf522f855cd5ee0
Tony Jenkins <tony@tony-jenkins.co.uk>
2024-07-22 13:20:55 +0100

    Tweak models

--- unipop/staff_list/models.py
+++ unipop/staff_list/models.py
@@ -5,16 +5,34 @@ class Room(models.Model):
     number = models.CharField(max_length=8)
     building = models.CharField(max_length=16)
 
+    class Meta:
+        ordering = [
+            'building',
+            'number',
+        ]
+        unique_together = [
+            'number',
+            'building',
+        ]
+
 
 class Faculty(models.Model):
     name = models.CharField(max_length=64)
     dean = models.CharField(max_length=64, null=True)
 
     class Meta:
-        verbose_name_plural='Faculties'
+        ordering = [
+            'name',
+        ]
+        verbose_name_plural = 'Faculties'
 
 
 class Tutor(models.Model):
     name = models.CharField(max_length=64)
-    faculty = models.ForeignKey(Faculty, on_delete=models.SET_NULL, null=True)
-    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
+    faculty = models.ForeignKey(Faculty, on_delete=models.SET_NULL, null=True, related_name='members')
+    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True, related_name='occupants')
+
+    class Meta:
+        ordering = [
+            'name',
+        ]
