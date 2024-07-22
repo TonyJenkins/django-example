06ec7066c6b03c36d373ccd5986dfe6164ae37e9
Tony Jenkins <tony@tony-jenkins.co.uk>
2024-07-22 11:09:27 +0100

    Initial model and migration

--- unipop/staff_list/models.py
+++ unipop/staff_list/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    number = models.CharField(max_length=8)
+    building = models.CharField(max_length=16)
+
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=64)
+    dean = models.CharField(max_length=64, null=True)
+
+
+class Tutor(models.Model):
+    name = models.CharField(max_length=64)
+    faculty = models.ForeignKey(Faculty, on_delete=models.SET_NULL, null=True)
+    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
