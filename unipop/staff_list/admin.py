743e5394fb8056dbf9bbb0567579a23bacdb0e1c
Tony Jenkins <tony@tony-jenkins.co.uk>
2024-07-22 14:59:45 +0100

    Enhance Tutor display in admin

--- unipop/staff_list/admin.py
+++ unipop/staff_list/admin.py
@@ -4,4 +4,24 @@ from .models import Room, Faculty, Tutor
 
 admin.site.register(Room)
 admin.site.register(Faculty)
-admin.site.register(Tutor)
+
+
+@admin.register(Tutor)
+class TutorAdmin(admin.ModelAdmin):
+
+    list_display = [
+            'name',
+            'room',
+            'faculty',
+        ]
+
+    list_filter = [
+        'room',
+        'faculty',
+    ]
+
+    ordering = [
+        'name',
+    ]
+
+    show_facets = admin.ShowFacets.ALWAYS
