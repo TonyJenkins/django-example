a8afe3a14c13765c7442b5c26784f64c39acfec3
Tony Jenkins <tony@tony-jenkins.co.uk>
2024-07-25 12:06:47 +0100

    Add detail page, link from main list

--- unipop/staff_list/urls.py
+++ unipop/staff_list/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
          views.all_staff_list,
          name='all_staff_list'
          ),
-    ]
+    path('<int:tutor_id>/',
+         views.staff_detail,
+         name='staff_detail.html'
+         ),
+]
--- unipop/staff_list/views.py
+++ unipop/staff_list/views.py
@@ -12,3 +12,14 @@ def all_staff_list(request):
     }
 
     return render(request, 'staff_list/all_staff_list.html', context,)
+
+
+def staff_detail(request, tutor_id):
+
+    staff_details = Tutor.objects.get(pk=tutor_id)
+
+    context = {
+        'staff_details': staff_details,
+    }
+
+    return render(request, 'staff_list/staff_detail.html', context)
